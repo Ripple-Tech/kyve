c37b986ea532909411621e2fc9cb7936e2c241dd
Ripple-Tech <ifeanyiinnocentokoro@gmail.com>
2025-09-04 15:48:54 +0100

    gg

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,19 +1,32 @@
 import Link from "next/link"
 import HeroForm from "./heroform"
 import { Button } from "./ui/button"
+import Image from "next/image"
 
 const Hero = () => {
   return (
     <div className="bg-kyve-hero min-h-screen w-full flex flex-1  py-10 gap-10 flex-col lg:flex-row">
       <div className="flex flex-col md:flex-1 text-left px-10  mt-10">
         <h1 className="text-4xl md:text-5xl font-bold text-golden-dark">The Escrow Platform You Can <br/> Trust</h1>
-        <p className="mt-4 text-lg text-foreground">Trade Safely, Kyve is your digital Shield...</p>
-         <Button 
-    asChild
-    className="mt-8 w-fit bg-primary text-golden-dark hover:bg-primary/90 font-bold px-10 md:px-12 py-5 rounded-lg"
-  >
-    <Link href="/explore">Explore</Link>
-  </Button>
+        
+     
+   {/* Store buttons */}
+        <div className="flex flex-col gap-4 mt-8">
+          <Image
+            src="/playstore.png"
+            alt="Get it on Google Play"
+            width={260}
+            height={100}
+            className="cursor-pointer"
+          />
+          <Image
+            src="/appstore.png"
+            alt="Download on the App Store"
+            width={260}
+            height={100}
+            className="cursor-pointer"
+          />
+        </div>
       </div>
 
       <div className="flex md:flex-1 py-10 px-3">
