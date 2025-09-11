fdb677f615936e201e3795fa07521656f180c1e8
Ripple-Tech <ifeanyiinnocentokoro@gmail.com>
2025-09-11 19:22:54 +0100

    hh

--- components/dashboard-page.tsx
+++ components/dashboard-page.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode, useState } from "react"
 import { Button } from "./ui/button"
-import { ArrowLeft } from "lucide-react"
+import { ArrowLeft, Plus } from "lucide-react"
 import { Heading } from "./heading"
 import { useRouter } from "next/navigation"
 import { Modal } from "./ui/modal"
@@ -47,13 +47,29 @@ export const DashboardPage = ({
         <div className="flex items-center gap-3">
           {cta}
           {showCreate && (
+            <>
             <Button
               size="sm"
-              className="bg-primary text-white hover:bg-amber-800"
+              className="hidden md:block bg-primary text-white hover:bg-amber-800"
               onClick={() => setShowCreateModal(true)}
             >
               + Create Escrow
             </Button>
+
+         {/* Mobile floating button */}
+<button
+          className="flex md:hidden fixed bottom-20 right-8 z-20 items-center justify-center h-16 w-16 rounded-full bg-primary text-white shadow-lg hover:bg-amber-700 transition-colors duration-200"
+          onClick={() => setShowCreateModal(!showCreateModal)}
+        >
+          {/*
+            The Plus icon from lucid-react.
+            We apply the 'h-12' and 'w-12' classes directly to the icon component.
+            These classes are used to increase the icon's size.
+          */}
+          <Plus className="h-12 w-12" />
+        </button>
+
+          </>
           )}
         </div>
       </div>
