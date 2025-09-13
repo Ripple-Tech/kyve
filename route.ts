c6d7186fc5bf185655ffff004d03e09990404950
Ripple-Tech <ifeanyiinnocentokoro@gmail.com>
2025-09-13 16:56:09 +0100

    flutterwave

--- route.ts
+++ route.ts
@@ -13,7 +13,7 @@ export const publicRoutes = [
     "/terms",
     "/api/paystack/webhook",
     "/api/paystack-callback",
-    "/api/flutterwave/webhook",
+    "/api/flutterwave/weebhook",
     //"/search",
     "/sitemap",
     "/sitemap.xml",
